5487bc9f78d90decb6b9e37f0f3dbac13fb5d64b
Robzimpulse <kugelfang.killaruna@gmail.com>
2020-09-02 22:24:24 +0700

    unfinish migrate gulp and npm dep

--- gulp/docs.js
+++ gulp/docs.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var gulp = require('gulp');
+let gulp = require('gulp');
 
-var $ = require('gulp-load-plugins')();
+let $ = require('gulp-load-plugins')();
 
 gulp.task('wintersmith-generate', $.shell.task([
-  'wintersmith build'
+    'wintersmith build'
 ], { cwd: 'docs' }));
 
-gulp.task('deploy-docs', ['wintersmith-generate'], function() {
-  return gulp.src('./docs/build/**/*')
-      .pipe($.ghPages());
-});
+// gulp.task('deploy-docs', ['wintersmith-generate'], function() {
+//   return gulp.src('./docs/build/**/*')
+//       .pipe($.ghPages());
+// });
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 'use strict';
 
-var gulp = require('gulp');
-var wrench = require('wrench');
+let gulp = require('gulp');
 
 /**
  *  This will load all js or coffee files in the gulp directory
  *  in order to load all gulp tasks
  */
-wrench.readdirSyncRecursive('./gulp').filter(function(file) {
-  return (/\.(js|coffee)$/i).test(file);
-}).map(function(file) {
-  require('./gulp/' + file);
-});
-
+require('./gulp/images');
+require('./gulp/styles');
+require('./gulp/scripts');
+require('./gulp/inject');
+require('./gulp/build');
 
 /**
  *  Default task clean temporaries directories and launch the
  *  main optimization build task
  */
-gulp.task('default', ['clean'], function () {
-  gulp.start('build');
-});
+// gulp.task('default', ['clean'], function () {
+//   gulp.start('build');
+// });
+
+gulp.task('default', gulp.series(['clean', 'build']));
